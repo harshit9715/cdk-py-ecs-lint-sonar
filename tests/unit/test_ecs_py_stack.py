2e4db1a3a5a04e8dc04a95969376a82432dd3565
harshit9715 <harshit9715@gmail.com>
2023-01-21 05:06:02 +0530

    chore: ci added tests

--- tests/unit/test_ecs_py_stack.py
+++ tests/unit/test_ecs_py_stack.py
@@ -5,14 +5,22 @@ from ecs_py.ecs_py_stack import EcsPyStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in ecs_py/ecs_py_stack.py
+app = core.App()
+stack = EcsPyStack(app, "ecs-py")
+template = assertions.Template.from_stack(stack)
 
 
-def test_sqs_queue_created():
-    app = core.App()
-    stack = EcsPyStack(app, "ecs-py")
-    template = assertions.Template.from_stack(stack)
+def test_vpc_created():
+    template.resource_count_is("AWS::EC2::VPC", 1)
 
-    template.to_json()
-    # template.has_resource_properties("AWS::SQS::Queue", {
-    #     "VisibilityTimeout": 300
-    # })
+
+def test_nat_created():
+    template.resource_count_is("AWS::EC2::NatGateway", 2)
+
+
+def test_igw_created():
+    template.resource_count_is("AWS::EC2::InternetGateway", 1)
+
+
+def test_subnets_are_created():
+    template.resource_count_is("AWS::EC2::Subnet", 4)
