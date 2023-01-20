cf1d9ba5dd0457ffb393544a534a2884f93f85cb
harshit9715 <harshit9715@gmail.com>
2023-01-21 03:26:13 +0530

    Initial commit

---
+++ tests/unit/test_ecs_py_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from ecs_py.ecs_py_stack import EcsPyStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in ecs_py/ecs_py_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = EcsPyStack(app, "ecs-py")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
